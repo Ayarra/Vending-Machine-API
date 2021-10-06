3a6d02fcf5408887454df2902742c01bc9754194
Houssam Hamdaoui <hamdaouihoussam.97@gmail.com>
2021-10-06 03:17:57 +0100

    Feat: Full CRUD operations for Users

--- components/User/userController.js
+++ components/User/userController.js
@@ -1,6 +1,24 @@
+const mongoose = require("mongoose");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const { User, validate } = require("./userModel");
+const { Product } = require("../Product/productModel");
+
+module.exports.getAllUsers = async (req, res) => {
+  const users = await User.find({}, "username").sort("username");
+  res.send(users);
+};
+
+module.exports.getUser = async (req, res) => {
+  if (mongoose.isValidObjectId(req.params.id)) {
+    const user = await User.findById(req.params.id);
+
+    if (!user)
+      return res.status(404).send("The user with the given ID was not found");
+
+    res.send(_.pick(user, ["_id", "username", "deposit"]));
+  } else res.status(400).send("Invalid user Id");
+};
 
 module.exports.userRegister = async (req, res) => {
   const { error } = validate(req.body);
@@ -21,4 +39,35 @@ module.exports.userRegister = async (req, res) => {
   res
     .header("x-auth-token", jwtToken)
     .send(_.pick(user, ["_id", "username", "deposit", "isSeller", "isBuyer"]));
+};
+
+module.exports.updateUser = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  if (mongoose.isValidObjectId(req.params.id)) {
+    const user = await User.findById(req.params.id);
+
+    if (!user)
+      return res.status(404).send("The user with the given ID was not found");
+
+    if (req.user._id === user._id.valueOf()) {
+      await User.updateOne(user, req.body);
+      res.send(_.pick(user, ["username", "deposit"]));
+    } else res.status(400).send("Not valid user");
+  } else res.status(400).send("Invalid user Id");
+};
+
+module.exports.deleteUser = async (req, res) => {
+  if (mongoose.isValidObjectId(req.params.id)) {
+    const user = await User.findById(req.params.id);
+
+    if (!user)
+      return res.status(404).send("The user with the given ID was not found");
+
+    if (req.user._id === user._id.valueOf()) {
+      await Product.deleteMany({ sellerId: req.user._id });
+      await User.deleteOne(user);
+      res.send(_.pick(user, ["username", "deposit"]));
+    } else res.status(400).send("Not valid user");
+  } else res.status(400).send("Invalid user Id");
 };
--- components/User/userRoute.js
+++ components/User/userRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const userController = require("./userController");
-
+const auth = require("../Auth/authMiddleware");
+const seller = require("../Auth/sellerMiddleware");
 const router = express.Router();
 
+router.get("/", auth, userController.getAllUsers);
+router.get("/:id", auth, userController.getUser);
 router.post("/", userController.userRegister);
+router.put("/:id", auth, userController.updateUser);
+router.delete("/:id", auth, userController.deleteUser);
 
 module.exports = router;
